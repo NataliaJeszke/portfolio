361487dc9d2cf123455e0e88d28ffbaa25368a05
NataliaJeszke <nataliajeszke8@gmail.com>
2023-07-13 18:37:20 +0200

    social btns, contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { PageWrapper } from "../components/page-wrapper";
 import ThankYou from "./thankyou";
 
 const Contact = () => {
   const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     setSubmitted(true);
   };
 
@@ -24,7 +23,17 @@ const Contact = () => {
           {submitted ? (
             <ThankYou />
           ) : (
-            <form action="#" className="space-y-8" onSubmit={handleSubmit}>
+            <form
+              action="https://formsubmit.co/el/pubefa"
+              method="POST"
+              className="space-y-8"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="hidden"
+                name="_subject"
+                value="New email from Portfolio"
+              />
               <div>
                 <label
                   htmlFor="email"
@@ -34,6 +43,7 @@ const Contact = () => {
                 </label>
                 <input
                   type="email"
+                  name="email"
                   id="email"
                   className="shadow-sm bg-green-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                   placeholder="name@email.com"
@@ -49,6 +59,7 @@ const Contact = () => {
                 </label>
                 <input
                   type="text"
+                  name="subject"
                   id="subject"
                   className="block p-3 w-full text-sm text-black bg-green-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                   placeholder="Subject"
@@ -64,6 +75,7 @@ const Contact = () => {
                 </label>
                 <textarea
                   id="message"
+                  name="message"
                   className="block p-2.5 w-full text-sm text-black bg-green-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                   placeholder="Leave a comment..."
                 ></textarea>
--- app/main/buttonsSocial/buttonsSocial.tsx
+++ app/main/buttonsSocial/buttonsSocial.tsx
@@ -1,7 +1,33 @@
+import linkedin from "../../icon/linkedin.svg";
+import pintrest from "../../icon/square-pinterest.svg";
+import github from "../../icon/square-github.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function ButtonsSocial() {
-    return (
-        <div>
-            <h1>ButtonsSocial</h1>
-        </div>
-    )
+  return (
+    <div className="flex flex-row flex-wrap gap-3 justify-center mt-2">
+      <Link href={"https://www.linkedin.com/in/natalia-jeszke/"}>
+        <Image
+          src={linkedin}
+          alt="LinkedIn"
+          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        />
+      </Link>
+      <Link href={"https://pin.it/2nJI2tm"}>
+        <Image
+          src={pintrest}
+          alt="LinkedIn"
+          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        />
+      </Link>
+      <Link href={"https://github.com/NataliaJeszke"}>
+        <Image
+          src={github}
+          alt="LinkedIn"
+          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        />
+      </Link>
+    </div>
+  );
 }
